58a37f35147b2b79db25995398ea2908afd60020
Johannes Hubertz <johannes@hubertz.de>
2017-11-03 16:05:25 +0100

    Replaces nosetests by pytest for flexibility

--- setup.py
+++ setup.py
@@ -1,35 +1,54 @@
+import os
 import sys
 from setuptools import setup, find_packages
+
+
+if sys.argv[-1] == 'test':
+    test_requirements = [
+        'pytest',
+        'flake8',
+        'coverage'
+    ]
+    try:
+        modules = map(__import__, test_requirements)
+    except ImportError as err:
+        err_msg = err.message.replace("No module named ", "")
+        msg = "%s is not installed. Install your test requirments." % err_msg
+        raise ImportError(msg)
+    os.system('py.test')
+    sys.exit()
 
 
 setup(name='iptables-converter',
       description='convert set of iptables-commands to iptables-save format',
       long_description=open('README.md').read(),
-      version='0.9.10',
-      license='GNU General Public License version 3 (or later)',
+      version='0.9.9.dev2',
+      license='dual: GNU General Public License version 3 (or later), '
+              'Apache Software License',
       platforms=['Linux', ],
       author='Johannes Hubertz',
       author_email='johannes@hubertz.de',
       url='https://github.com/sl0/conv.git',
       classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: '
-        'GNU General Public License v3 or later (GPLv3+)',
-        'Intended Audience :: System Administrators',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Topic :: Security',
-        'Topic :: System :: Networking :: Firewalls',
-        'Topic :: Utilities',
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: '
+          'GNU General Public License v3 or later (GPLv3+)',
+          'License :: OSI Approved :: Apache Software License',
+          'Intended Audience :: System Administrators',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Security',
+          'Topic :: System :: Networking :: Firewalls',
+          'Topic :: Utilities',
       ],
       packages=find_packages(exclude=['tests', ]),
       entry_points={
-        'console_scripts': [
-            'iptables_converter=iptables_conv.iptables_converter:main',
-            'ip6tables_converter=iptables_conv.iptables_converter:main',
-        ],
+          'console_scripts': [
+              'iptables-converter=iptables_conv.iptables_converter:main',
+              'ip6tables-converter=iptables_conv.iptables_converter:main',
+          ],
       },
       test_suite='nose.collector',
       tests_require=['nose'],
-      )
+     )
