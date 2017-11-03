cd3d5512be734d170750d18d5ee6045ab8a7865e
Johannes Hubertz <johannes@hubertz.de>
2017-11-03 16:07:22 +0100

    Corrects License info in python module

--- iptables_conv/__init__.py
+++ iptables_conv/__init__.py
@@ -3,7 +3,7 @@
 # (C) 2016,2017 Johannes Hubertz <johannes@hubertz.de>
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
+#    the Free Software Foundation; either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -14,3 +14,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, please see
 #    <http://www.gnu.org/licenses/>
+#
+#    Additionally this program is licensed under the terms of the
+#    Apache License, Version 2, see the top level directory right here,
+#    or you can view there:  <http://www.apache.org/licenses/>
